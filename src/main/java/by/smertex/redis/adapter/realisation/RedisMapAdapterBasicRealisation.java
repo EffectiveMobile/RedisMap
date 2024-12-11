d3c169ecadc455bb6807375b5ee0ddf6283a28ab
Smertex <kiratsvetkova102@gmail.com>
2024-12-11 15:53:40 +0300

    Refactoring

--- src/main/java/by/smertex/redis/adapter/realisation/RedisMapAdapterBasicRealisation.java
+++ src/main/java/by/smertex/redis/adapter/realisation/RedisMapAdapterBasicRealisation.java
@@ -2,7 +2,7 @@ package by.smertex.redis.adapter.realisation;
 
 import redis.clients.jedis.Jedis;
 
-public final class RedisMapAdapterBasicRealisation extends AbstractRedisMapAdapter{
+public final class RedisMapAdapterBasicRealisation extends AbstractRedisMapAdapter {
     public RedisMapAdapterBasicRealisation(Jedis jedis) {
         super(jedis);
     }
